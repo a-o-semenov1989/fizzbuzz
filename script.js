1a640c3795e0c7fb7d20f0ca140838c38c1cbfb6
Anatolii Semenov <a-o-semenov1989@gmail.com>
2020-08-07 12:27:34 +0300

    intial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const fizzBuzz = (num) => {
+  for (let i = 1; i <= num; i++) {
+    // If number equals 3 and 5
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('fizzbuzz');
+    } //If number equals 3
+      else if (i % 3 === 0) {
+        console.log('fizz');
+      } // If number equals 5
+        else if (i % 5 === 0) {
+          console.log('buzz');
+      } else {
+        console.log(i);
+      }
+  }
+};
+
+fizzBuzz(45);
